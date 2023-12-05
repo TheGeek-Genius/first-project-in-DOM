89c39b721c076f826374a7e398f53b8b0cb0ee1a
TheGeek-Genius <Arehmana416@gmail.com>
2023-12-05 15:39:20 +0500

    my first project in DOM

---
+++ app.js
@@ -0,0 +1,90 @@
+const SelectTaskForm = document.querySelector("#task-form");
+
+// console.log(SelectTaskForm, 'SelectTaskForm');
+
+SelectTaskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // console.log(event, "submit form");
+
+    const selectTaskInputField = document.querySelector("#task");
+    // console.log(selectTaskInputField, "selectTaskInputField");
+    const taskInputValue = selectTaskInputField.value;
+    // console.log("working good")
+
+    if(!taskInputValue){
+        alert("please fill the input field");
+        return
+    }    
+    // console.log(taskInputValue, "taskInputValue");
+
+    // return;
+    const collection = document.querySelector(".collection");
+    //   console.log(collection, "collection");
+    
+    const createLiElement = document.createElement("li");
+    createLiElement.className = "collection-item";
+    createLiElement.innerHTML = ` ${taskInputValue}
+        <a href="#" class="delete-item secondary-content">
+        <i class="fa fa-remove"></i>
+        </a>`;
+    
+    collection.append(createLiElement);
+   
+    selectTaskInputField.value = "" ;
+    // console.log(createLiElement, "createLiElement");
+    
+});
+
+    const collectionItem = document.querySelector(".collection");
+    // console.log(collectionItem, "collectionItem");
+
+collectionItem.addEventListener("click", function(event){
+    event.preventDefault();
+    if(event.target.className === "fa fa-remove"){
+         // console.log("task is delete");
+        if(confirm("Are You Sure")) {
+            event.target.parentElement.parentElement.remove();
+        }
+    }
+});
+
+const clearTaskBtn = document.querySelector(".clear-tasks");
+// console.log(clearTaskBtn, "clearTaskBtn");
+
+clearTaskBtn.addEventListener("click", function(){
+    // console.log("its working");
+    const collection = document.querySelector(".collection");
+    if(confirm("Are you Sure")){
+        collection.innerHTML = "" ;
+    }
+})
+
+const taskFilter = document.querySelector("#filter");
+// console.log(taskFilter, "taskFilter");
+
+taskFilter.addEventListener("keyup", function(event){
+    const currentElement = event.target;
+    const filterInputValue = currentElement.value;
+    const allLiSelect = document.querySelectorAll(".collection-item");
+    console.log(allLiSelect, "allLiSelect")
+    // console.log(filterInputValue,"filterInputValue")   
+
+    allLiSelect.forEach( function (singleLiElement) {
+        
+
+        if (
+            singleLiElement.innerText.toLowerCase().indexOf(filterInputValue.toLowerCase()) === -1   
+          )
+        
+          {
+            singleLiElement.style.display = "none";
+          } 
+          else {
+            singleLiElement.style.display = "block";
+          }
+        
+    });
+});
+
+
+
